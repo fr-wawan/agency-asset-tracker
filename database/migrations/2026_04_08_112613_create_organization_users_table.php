faa4ab8911ba64232efcab032abbf68d4c4c5e5c
Wawan Cihuy <hermawantan12@gmail.com>
2026-04-08 19:30:56 +0800

    feat: migrations organization

---
+++ database/migrations/2026_04_08_112613_create_organization_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('organization_users', function (Blueprint $table) {
+            $table->ulid('id')->primary();
+            $table->foreignUlid('organization_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignUlid('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->string('role');
+            $table->foreignUlid('invited_by')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete();
+            $table->timestamp('joined_at')
+                ->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('organization_users');
+    }
+};
